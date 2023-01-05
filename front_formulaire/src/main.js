f3177cccfee7cc5047b1892db2d4a340c9482f3d
Dan <dan.esposito@ynov.com>
2023-01-05 15:55:20 +0100

    ajout du docker-compose avec les env sur le front pour le sso

--- front_formulaire/src/main.js
+++ front_formulaire/src/main.js
@@ -5,16 +5,19 @@ import vuetify from './plugins/vuetify';
 import "vuetify/dist/vuetify.min.css";
 import VueCookies from 'vue-cookies'
 import { Auth0Plugin } from './auth';
-import { domain, clientId, audience } from '../auth_config.json';
+import dotenv from 'dotenv'
+
+dotenv.config()
+// import { domain, clientId, audience } from '../auth_config.json';
 Vue.config.productionTip = false;
 
 
 // Install the authentication plugin here
 Vue.use(VueCookies)
 Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
-  audience,
+  domain:process.env.VUE_APP_API_DOMAIN,
+  clientId:process.env.VUE_APP_API_CLIENT_ID,
+  audience:process.env.VUE_APP_API_AUDIENCE,
   onRedirectCallback: appState => {
     router.push(
       appState && appState.targetUrl
